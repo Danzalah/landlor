81de0a73f62d274c4001d0bce1d3853ab9a84b02
Huy Le <huyftw@aim.com>
2022-04-27 18:20:30 -0400

    test

--- public/javascripts/whatscooking.js
+++ public/javascripts/whatscooking.js
@@ -12,24 +12,14 @@ function success() {
 
     let element = (
         <div>
-<<<<<<< HEAD
-
             <ul className="homeBar">
                 <li><img className="homeLogo" src="../images/logo_large.png" width="400" /></li>
-
                 <li>
                     <div className="bar">
                         <input className="searchbar" type="text" name="ingredients" placeholder="Whats Cooking?" />
                     </div>
                 </li>
             </ul>
-=======
-            <div class="bar">
-            <form action="whatscooking" method="post" id="submit_ingredients">
-            <input className="searchbar" type="text" name="ingredients" placeholder="Enter Ingredients and seperate by commas (eg. milk,fish)" />            
-            </form>
-            </div>
->>>>>>> bc9efb7552dc8341ab58b663f1e439d30cffd9f3
         </div>
     );
 
